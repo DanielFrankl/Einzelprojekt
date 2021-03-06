c5a68645994c89f3ec2a8255ead0b26937fecf47
DanielFrankl <danielfra@edu.aau.at>
2021-03-06 10:53:19 +0100

    erste Version TCPClient

--- app/src/main/java/com/example/einzelprojekt/TCPClient.java
+++ app/src/main/java/com/example/einzelprojekt/TCPClient.java
@@ -1,4 +1,30 @@
 package com.example.einzelprojekt;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
 public class TCPClient {
+
+    public static void main(String[] args) throws Exception {
+        int matrikelnummer;
+
+        BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
+
+        Socket clientSocket = new Socket("hostname", 53212);
+
+        DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
+
+        BufferedReader inFromServer = new BufferedReader(new BufferedReader(new InputStreamReader(clientSocket.getInputStream())));
+
+        matrikelnummer = inFromUser.read();
+
+        outToServer.writeByte(matrikelnummer+'\n');
+
+        System.out.println("FROM SERVER: " + matrikelnummer);
+
+        clientSocket.close();
+    }
+
 }
